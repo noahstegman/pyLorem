bd9c882a60319c2b0e943f9e9d09cfa3069f06ee
stegmannoah@gmail.com <stegmannoah@gmail.com>
2023-01-26 17:47:48 -0800

    pyLorem first commit

---
+++ pyLorem.py
@@ -0,0 +1,9 @@
+import re
+import random
+import sys
+
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+
+output = re.findall(r"\w+", lorem)
+count = sys.argv[1] if len(sys.argv) >= 2 else 100
+for i in range(1, int(count)): print(random.choice(output), end=" ")
